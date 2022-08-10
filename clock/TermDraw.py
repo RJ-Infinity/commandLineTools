3d7a2302d69b882422c112b1cead65d3b2d4c2da
RJ_Infinity <rjinfinity2004@gmail.com>
2022-08-10 14:57:37 +0100

    much better line algorithm now live working

--- clock/TermDraw.py
+++ clock/TermDraw.py
@@ -1,6 +1,8 @@
+from argparse import ArgumentError
 import math
 import sys
 from turtle import clone
+from TUI import Seq
 map = lambda value,fromMin,fromMax,toMin,toMax: toMin+(((value-fromMin)/(fromMax-fromMin))*(toMax-toMin))
 
 class Pixels:
@@ -24,19 +26,55 @@ class Pixels:
 			for x in range(diameter):
 				self.__pixels[y][x] = x == value1 or x == value2
 		return self
-	def drawLine(self,x1,y1,x2,y2):
-		if x1>x2:
-			x2,x1=x1,x2
+	# def drawLine(self,x1,y1,x2,y2):
+	# 	if (
+	# 		x1<0 or
+	# 		x2<0 or
+	# 		y1<0 or
+	# 		y2<0 or
+	# 		x1>len(self.__pixels[0]) or
+	# 		x2>len(self.__pixels[0]) or
+	# 		y1>len(self.__pixels) or
+	# 		y2>len(self.__pixels)
+	# 	):
+	# 		raise ValueError("the argumnets can not be less than 0 or larger than the size of the Pixels")
 
-		if y1>y2:
-			y2,y1=y1,y2
+		# if x1>=x2:
+			# x2,x1=x1,x2
 
-		dx = x2 - x1
-		dy = y2 - y1
+		# # if y1>y2:
+		# # 	y2,y1=y1,y2
 
-		for x in range(x1, x2):
-			y = y1 + dy * (x - x1) / dx
-			self.__pixels[int(y+0.5)][int(x+0.5)] = 1
+		# dx = x2 - x1
+		# dy = y2 - y1
+
+		# for x in range(x1, x2):
+		# 	y = y1 + dy * (x - x1) / dx
+		# 	self.__pixels[int(y+0.5)][int(x+0.5)] = 1
+	def drawLine(self,x0,y0,x1,y1):
+		dx = abs(x1 - x0)
+		sx = 1 if x0 < x1 else -1
+		dy = -abs(y1 - y0)
+		sy = 1 if y0 < y1 else -1
+		error = dx + dy
+		
+		while True:
+			try:
+				self.__pixels[y0][x0]=1
+			except IndexError:pass
+			if x0 == x1 and y0 == y1:
+				break
+			e2 = 2 * error
+			if e2 >= dy:
+				if x0 == x1:
+					break
+				error = error + dy
+				x0 = x0 + sx
+			if e2 <= dx:
+				if y0 == y1:
+					break
+				error = error + dx
+				y0 = y0 + sy
 
 	def drawPos(self,x,y,value):
 		self.__pixels[y][x] == value
--- clock/face.py
+++ clock/face.py
@@ -1,17 +1,58 @@
 import math
-import os
+import signal
+from time import sleep
+from TUI import Seq
 import sys
-import timeit
+import datetime
 import TermDraw
+import helpers
+
 
 CHAR_WIDTH, CHAR_HEIGHT = 8, 18
+
+CrtlC = False
 
 def main(args):pass
 
 if __name__ == "__main__":
-	SIZE = 80
-	face = TermDraw.Pixels(SIZE*CHAR_WIDTH,SIZE*CHAR_WIDTH)
+	live = True
+	def handler(signum, frame):
+		global CrtlC
+		if signum == 2:
+			CrtlC = True
+
+	signal.signal(signal.SIGINT, handler)
+
+
+	helpers.EnableVirtualTerminalProcessing()
+
+	SIZE = 40
+	face = TermDraw.Pixels(SIZE*CHAR_HEIGHT,SIZE*CHAR_HEIGHT)
 	face.drawCircle()
-	img = face.clone()
-	img.drawLine(SIZE//2*CHAR_WIDTH,SIZE//2*CHAR_WIDTH,100,100)
-	img.renderPixels(CHAR_WIDTH,CHAR_HEIGHT)
+
+	
+	r = SIZE//2*CHAR_HEIGHT
+	sys.stdout.write(Seq.CursorDown(SIZE+1))
+
+	while True:
+		now = datetime.datetime.now().replace(hour=3)
+		
+		hourX=int((math.sin(math.radians((now.hour*30)%360))*(r-(CHAR_HEIGHT*5))+r))
+		hourY=r-int(math.cos(math.radians((now.hour*30)%360))*(r-(CHAR_HEIGHT*5)))
+		
+		minX=int((math.sin(math.radians((now.minute*6)%360))*(r)+r))
+		minY=r-int(math.cos(math.radians((now.minute*6)%360))*(r))
+
+		secX=int((math.sin(math.radians((now.second*6)%360))*(r)+r))
+		secY=r-int(math.cos(math.radians((now.second*6)%360))*(r))
+
+		sys.stdout.write(Seq.CursorUp(SIZE+1))
+
+		img = face.clone()
+		img.drawLine(r,r,hourX,hourY)
+		img.drawLine(r,r,minX,minY)
+		img.drawLine(r,r,secX,secY)
+		img.renderPixels(CHAR_WIDTH,CHAR_HEIGHT)
+		if (not live) or CrtlC:break
+		sleep(0.9)
+		if (not live) or CrtlC:break
